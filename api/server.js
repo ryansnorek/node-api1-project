fd02c9bb7e67679ead8c308d9de236fb56e50e8f
Ryan <snorekr@gmail.com>
2021-11-29 13:38:51 -0500

    error messages

--- api/server.js
+++ api/server.js
@@ -8,12 +8,19 @@ server.use(express.json());
 // POST	/api/users
 server.post('/api/users', async (req, res) => {
     try {
-        const newUser = await Users.insert(req.body);
-        res.status(201).json(newUser);
+        if (!req.body.name || !req.body.bio) {
+            res.status(400).json({ 
+                message: "Please provide name and bio for the user"
+            })
+        }
+        else {
+            const newUser = await Users.insert(req.body);
+            res.status(201).json(newUser);
+        }
 
     }   catch (err) {
         res.status(500).json({
-            message: "you broke it",
+            message: "There was an error while saving the user to the database",
             error: err.message
         })
     }
@@ -26,7 +33,7 @@ server.get('/api/users', async (req, res) => {
 
     } catch (err) {
         res.status(500).json({
-            message: "you broke it",
+            message: "The users information could not be retrieved",
             error: err.message
         });
     }
@@ -34,18 +41,34 @@ server.get('/api/users', async (req, res) => {
 // GET	/api/users/:id
 server.get('/api/users/:id', async (req, res) => {
     try {
-        const user = await Users.findById(req.params.id);
-        res.json(user);
+        if (!req.body.name) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        }
+        else {
+            const user = await Users.findById(req.params.id);
+            res.json(user);
+        }
     } catch (err) {
         res.status(500).json({
-            message: "you broke it",
+            message: "The user information could not be retrieved",
             error: err.message
         });
     }
 })
-
 // DELETE	/api/users/:id
+server.delete('/api/users/:id', async (req, res) => {
+    try {
+        const deleteUser = await Users.remove(req.body);
 
+    } catch (err) {
+        res.status(500).json({
+            message: "you broke it",
+            error: err.message
+        })
+    }
+})
 // PUT	/api/users/:id
 
 
