1673c3e20b361d80df8c97d1f2bfbbba47da15f4
Ryan <snorekr@gmail.com>
2021-11-29 13:05:30 -0500

    get users

--- api/server.js
+++ api/server.js
@@ -1,11 +1,37 @@
 const express = require('express');
+const Users = require('./users/model');
 
 const server = express();
 
 server.use(express.json());
 
-server.get('/', (req, res) => {
-    res.json({ message: 'hello' })
-});
+// POST	/api/users
+
+// GET	/api/users
+server.get('/api/users', async (req, res) => {
+    try {
+        const users = await Users.find();
+        res.json(users);
+    }
+    catch (err) {
+        res.status(500).json({
+            message: "you broke it",
+            error: err.message
+        })
+    }
+})
+
+// GET	/api/users/:id
+
+// DELETE	/api/users/:id
+
+// PUT	/api/users/:id
+
+
+// {
+//     id: "a_unique_id", // String, required
+//     name: "Jane Doe",  // String, required
+//     bio: "Having fun", // String, required
+//   }
 
 module.exports = server;
