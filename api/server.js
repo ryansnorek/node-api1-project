b90a6681bf961c90c24d33384a45ef0233bfdf1b
Ryan <snorekr@gmail.com>
2021-11-29 18:43:59 -0500

    syntax experiment

--- api/server.js
+++ api/server.js
@@ -7,21 +7,18 @@ server.use(express.json());
 
 // INSERT NEW USER
 server.post('/api/users', async (req, res) => {
+    if (!req.body.name || !req.body.bio) {
+        return res.status(400).json({ 
+            message: "Please provide name and bio for the user"
+        });
+    }
     try {
-        if (!req.body.name || !req.body.bio) {
-            res.status(400).json({ 
-                message: "Please provide name and bio for the user"
-            });
-        }
-        else {
-            const newUser = await Users.insert(req.body);
-            res.status(201).json(newUser);
-        }
-
-    }   catch (err) {
+        const newUser = await Users.insert(req.body);
+        res.status(201).json(newUser);
+    } catch (e) {
         res.status(500).json({
             message: "There was an error while saving the user to the database",
-            error: err.message
+            error: e.message
         });
     }
 })
@@ -30,53 +27,44 @@ server.get('/api/users', async (req, res) => {
     try {
         const users = await Users.find();
         res.json(users);
-
-    } catch (err) {
+    } catch (e) {
         res.status(500).json({
             message: "The users information could not be retrieved",
-            error: err.message
+            error: e.message
         });
     }
 })
 // GET USER BY ID //
 server.get('/api/users/:id', async (req, res) => {
-    const { id } = req.params;
     try {
-        const user = await Users.findById(id);
-        if (!user) {
-            res.status(404).json({
-                message: "The user with the specified ID does not exist"
-            });
-        }
-        else {
-            res.json(user);
-        }
-    } catch (err) {
+        const user = await Users.findById(req.params.id);
+
+        if (user) return res.json(user);
+
+        return res.status(404).json({
+            message: "The user with the specified ID does not exist"
+        });
+    } catch (e) {
         res.status(500).json({
             message: "The user information could not be retrieved",
-            error: err.message
+            error: e.message
         });
     }
 })
 // REMOVE USER //
 server.delete('/api/users/:id', async (req, res) => {
-    const { id } = req.params;
-
     try {
-        const deleteUser = await Users.remove(id);
-        if (!deleteUser) {
-            res.status(404).json({
-                message: "The user with the specified ID does not exist"
-            });
-        }
-        else {
-            res.json(deleteUser);
-        }
+        const deleteUser = await Users.remove(req.params.id);
 
-    } catch (err) {
+        if (deleteUser) return res.json(deleteUser);
+
+        return res.status(404).json({
+            message: "The user with the specified ID does not exist"
+        });
+    } catch (e) {
         res.status(500).json({
             message: "The user could not be removed",
-            error: err.message
+            error: e.message
         });
     }
 })
@@ -85,25 +73,23 @@ server.put('/api/users/:id', async (req, res) => {
     const { id } = req.params;
     const { body } = req;
 
+    if (!body.name || !body.bio) {
+        return res.status(400).json({
+            message: "Please provide name and bio for the user"
+        });
+    }
     try {
         const updatedUser = await Users.update(id, body);
-        if (!updatedUser) {
-            res.status(404).json({
-                message: "The user with the specified ID does not exist"
-            });
-        } 
-        else if (!body.name || !body.bio) {
-            res.status(400).json({
-                message: "Please provide name and bio for the user"
-            });
-        }
-        else {
-            res.json(updatedUser);
-        }
-    } catch (err) {
+
+        if (updatedUser) return res.json(updatedUser);
+        
+        return res.status(404).json({
+            message: "The user with the specified ID does not exist"
+        });
+    } catch (e) {
             res.status(500).json({
             message: "The user information could not be modified",
-            error: err.message
+            error: e.message
         });
     }
 })
