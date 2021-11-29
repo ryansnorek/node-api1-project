2db25f8590b7d40e0c7afc7523f65f3e8b7c7acb
Ryan <snorekr@gmail.com>
2021-11-29 13:22:19 -0500

    insert new user

--- api/server.js
+++ api/server.js
@@ -6,12 +6,24 @@ const server = express();
 server.use(express.json());
 
 // POST	/api/users
+server.post('/api/users', async (req, res) => {
+    try {
+        const newUser = await Users.insert(req.body);
+        res.status(201).json(newUser);
 
+    }   catch (err) {
+        res.status(500).json({
+            message: "you broke it",
+            error: err.message
+        })
+    }
+})
 // GET	/api/users
 server.get('/api/users', async (req, res) => {
     try {
         const users = await Users.find();
         res.json(users);
+
     } catch (err) {
         res.status(500).json({
             message: "you broke it",
